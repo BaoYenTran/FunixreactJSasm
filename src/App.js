8a275115b5b9c9bdb25450b5a51ba17bcdcb84d0
Yen Tran <yentbFX14890@gmail.com>
2022-04-01 00:58:49 +0700

    complete split columns

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import {STAFFS} from './shared/staffs.jsx'
-import StaffList from './components/StaffListComponent'
+import { STAFFS } from './shared/staffs.jsx'
+import StaffList from './components/StaffListComponent';
+import { Navbar, NavbarBrand } from 'reactstrap';
 class App extends Component {
-  
-  constructor (props) {
+
+  constructor(props) {
     super(props);
     this.state = {
-      staffs: STAFFS
+      staffs: STAFFS,
+      className: "col-12 col-md-6 col-lg-4 mt-1"
     };
+    this.split2Columns = this.split2Columns.bind(this)
+    this.split3Columns = this.split3Columns.bind(this)
+    this.split6Columns = this.split6Columns.bind(this)
   }
-  render () {
-  return (
-    
-    <StaffList staffs={this.state.staffs}/>
-    
-  );
-};
+  split2Columns() {
+    this.setState({ className: "col-12 col-md-6 col-lg-6 mt-0" })
+  };
+
+  split3Columns() {
+    this.setState({ className: "col-12 col-md-6 col-lg-4 mt-0" })
+  };
+  split6Columns() {
+    this.setState({ className: "col-12 col-md-6 col-lg-2 mt-0" })
+  };
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand className="text-center" href="/">ỨNG DỤNG QUẢN LÝ NHÂN SỰ  </NavbarBrand>
+          </div>
+        </Navbar>
+        <div>
+                <button onClick={this.split2Columns} className="btn-warning">Chia 2 cột</button>
+                <button onClick={this.split3Columns} className="btn-danger">Chia 3 cột </button>
+                <button onClick={this.split6Columns} className="btn-info"  >Chia 6 cột </button>
+            </div>
+
+
+        <StaffList staffs={this.state.staffs} col={this.state.className} />
+      </div>
+    );
+  };
 }
 export default App;
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,80 +1,76 @@
-import React, { Component } from 'react';
-import { Navbar,NavbarBrand } from 'reactstrap';
-import { Card, CardTitle ,CardText  } from 'reactstrap';
-import dateFormat from 'dateformat'; 
+import React, { Component, Fragment } from 'react';
+
+import { Card, CardTitle, CardText } from 'reactstrap';
+import dateFormat from 'dateformat';
+
 class StaffList extends Component {
 
     constructor(props) {
-        super(props);   
+        super(props);
         this.state = {
             selectedStaff: null
         }
-        
+
     }
     onStaffSelect(staff) {
-        this.setState({selectedStaff: staff})
+        this.setState({ selectedStaff: staff })
     }
-    
-    renderStaff (staff) {
-        if (staff !=null ) {
+
+    renderStaff(staff) {
+        if (staff != null) {
             return (
-                <div className ="col-12 col-md-6 col-lg-4 mt-1">
-            <Card >
-                        
-                            <CardTitle> Họ và Tên : {staff.name} </CardTitle>
-                            <CardText> Ngày sinh : {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
-                            <CardText> Ngày vào công ty : {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
-                            <CardText> Phòng ban : {staff.department.name}</CardText>
-                            <CardText> Số ngày nghỉ còn lại : {staff.annualLeave}</CardText>
-                            <CardText> Số ngày đã làm thêm : {staff.overTime}</CardText>
-            </Card>
-            </div>
+                <div className="col-12 col-md-6 col-lg-4 mt-1">
+                    <Card >
+                        <CardTitle> Họ và Tên : {staff.name} </CardTitle>
+                        <CardText> Ngày sinh : {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+                        <CardText> Ngày vào công ty : {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
+                        <CardText> Phòng ban : {staff.department.name}</CardText>
+                        <CardText> Số ngày nghỉ còn lại : {staff.annualLeave}</CardText>
+                        <CardText> Số ngày đã làm thêm : {staff.overTime}</CardText>
+                    </Card>
+                </div>
             )
         }
         else {
             return (
-            <div></div>
+                <div></div>
             )
         }
     }
-    render () {
-        const staffList = this.props.staffs.map ( (staff) => {
+    render() {
+        console.log(this.props.col);
+       
+    
+        const staffList = this.props.staffs.map((staff) => {
             return (
+           
                 
-                    <div key ={staff.id} className ="col-12 col-md-6 col-lg-4 mt-1">
-                        <Card onClick = {() => this.onStaffSelect(staff)}>
-                            <CardTitle> {staff.name} </CardTitle>
-                        </Card> 
-                    </div>
-                        
-              
+
+                <div key={staff.id} className = {this.props.col}>
+                    <Card onClick={() => this.onStaffSelect(staff)}>
+                        <CardTitle className="text-center"> {staff.name} </CardTitle>
+                    </Card>
+                </div>
+   
+
+
             );
         });
 
-    return (
-        <div>           
-            
-            <Navbar dark color="primary">
-            <div className="container-fluid">
-                <NavbarBrand href="/"> Ứng dụng quản lý nhân sự V1.0 </NavbarBrand>
-                </div>
-            </Navbar>
-          {/* <ChiaCot/> */}
-            <div className ="container-fluid">
-                <div className ="row">
-                
-                    
-                    {staffList}
+        return (
+            <div>
+               
+                <div className="container-fluid">
+                    <div className="row">
+                        {staffList}
                     </div>
-                    <div className = "row">
-                   {this.renderStaff(this.state.selectedStaff)}
-                  
-                  
-                 </div>
-                
+                    <div className="row">
+                        {this.renderStaff(this.state.selectedStaff)}
+                    </div>
+
+                </div>
             </div>
-        </div>
- 
+
         );
     }
 }
